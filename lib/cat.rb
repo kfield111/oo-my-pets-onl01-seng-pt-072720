0eca57a5e520bc03351745e87345a84f9836170a
Kelin Field <kfield111@hotmail.com>
2020-08-25 21:12:11 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
 class Cat
-  # code goes here
+    attr_reader :name
+    attr_accessor :owner
+
+  def initialize (name, owner, mood= "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+
+
+    
 end

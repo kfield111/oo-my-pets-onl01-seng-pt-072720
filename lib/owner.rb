33a73d9c7d4a385270eb7fc22158f6c7678f35f9
Kelin Field <kfield111@hotmail.com>
2020-08-25 22:00:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -75,7 +75,7 @@ end
 def list_pets
   number_of_dogs = dogs.length
   number_of_cats = cats.length
-  puts "I have #{number_of_dogs} dog(s), and #{number_of_cats}."
+  puts "I have #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
 end
 
 
